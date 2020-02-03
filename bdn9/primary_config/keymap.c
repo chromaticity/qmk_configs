fcf4f2cda9fa0b06e3a0f457ef8d77e27207b682
Brandon Antillon <brandonantil457@gmail.com>
2020-02-03 12:40:22 -0800

    Added in my own personal config.h file for BDN9.

--- bdn9/primary_config/keymap.c
+++ bdn9/primary_config/keymap.c
@@ -1,10 +1,9 @@
 #include QMK_KEYBOARD_H
 
-#define MEDIA_KEY_DELAY 10
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
-        KC_MUTE, KC_PWR, KC_CALC,
+        KC_MPLY, KC_PWR, KC_CALC,
         KC_MPRV, KC_MPLY, KC_MNXT,
         BL_TOGG, KC_STOP, KC_F24
     )
@@ -15,9 +14,9 @@ void encoder_update_user(uint8_t index, bool clockwise) {
         /* Top-left encoder (volume) */
         case 0:
             if (clockwise) {
-                tap_code(KC_PGUP);
+                tap_code(KC_MNXT);
             } else {
-                tap_code(KC_PGDN);
+                tap_code(KC_MPRV);
             }
             break;
     }
