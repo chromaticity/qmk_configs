f7742a15d2a9861c355cda00d38131400c8ca337
Brandon Antillon <brandonantil457@gmail.com>
2020-02-03 12:47:08 -0800

    Removed config.h, moved into project dir

---
+++ bdn9/primary_config/config.h
@@ -0,0 +1,6 @@
+/* All of my custom configs on top of what is already
+* available with QMK config
+*/
+
+// Necessary for rotary encoders to work.
+#define TAP_CODE_DELAY 10
--- bdn9/primary_config/keymap.c
+++ bdn9/primary_config/keymap.c
@@ -1,17 +1,16 @@
 #include QMK_KEYBOARD_H
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(
         KC_MPLY, KC_PWR, KC_CALC,
-        KC_MPRV, KC_MPLY, KC_MNXT,
+        KC_MPRV, KC_MPLY, KC_F23,
         BL_TOGG, KC_STOP, KC_F24
     )
 };
 
 void encoder_update_user(uint8_t index, bool clockwise) {
     switch (index) {
-        /* Top-left encoder (volume) */
+        /* Top-left encoder, for prev/next song */
         case 0:
             if (clockwise) {
                 tap_code(KC_MNXT);
